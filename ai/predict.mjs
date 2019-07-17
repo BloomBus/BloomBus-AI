d085f8c2ef58850604c3ec55c6c507e68beb1f3f
michaelpodonnell <odonnellm56@gmail.com>
2019-07-17 12:26:32 -0400

    Add count of intersections and stops to input

--- ai/predict.mjs
+++ ai/predict.mjs
@@ -1,24 +1,39 @@
+'use strict';
+
 import { tensor1d, loadLayersModel } from '@tensorflow/tfjs-node-gpu'; // should likely change to tfjs-node for server deployment (function calls will remain the same)
-import { nearestPointOnLine, length, lineSlice } from '@turf/turf';
+import { booleanPointOnLine, nearestPointOnLine, length, lineSlice, _point } from '@turf/turf';
 
 /**
  * Predicts the arrival time of the given bus at the given bus stop.
- * 
+ *
  * @author Michael O'Donnell
  * @param {LineString} loop The loop the bus is on.
  * @param {Feature} bus The geojson of the bus.
  * @param {Point} busStop The location of the bus stop.
  * @returns {Number} The predicted arrival time.
  */
-async function predictArrivalTime(loop, bus, busStop) {
+async function predictArrivalTime(loop, stops, intersections, bus, busStop) {
 	const unitOfMeasurement = 'kilometers';
 	const busCoord = nearestPointOnLine(loop, bus.geometry.coordinates, {units: unitOfMeasurement});
 
+	const path = lineSlice(busCoord, busStop, loop);
+
+	let numOfStops = 0;
+	for (stop of stops)
+		if (booleanPointOnLine(_point(stop.geometry.coordinates), path))
+			numOfStops++;
+	numOfStops--; // remove target bus stop
+
+	let numOfIntersections = 0;
+	for (intersection in intersections)
+		if (booleanPointOnLine(_point(intersection.geometry.coordinates), path))
+			numOfIntersections++;
+
 	const model = await loadLayersModel('file:///home/nvidia/Documents/BloomBus/ai/model/model.json'/*, 'file:///home/nvidia/Documents/BloomBus/ai/model/something.bin'*/); // something.bin is the weights (might not be needed) (should load from server once set up)
 	let prediction = model.predict(tensor1d([
-		length(lineSlice(busCoord, busStop, loop), {units: unitOfMeasurement}),
-		// number of intersections from bus to bus stop,
-		// number of bus stops from bus to bus stop,
+		length(path, {units: unitOfMeasurement}),
+		numOfIntersections,
+		numOfStops,
 		bus.speed,
 		Date.now()
 	]));
