c7d543e5dd01e860220332a79bc0f03a48771e3a
michaelpodonnell <odonnellm56@gmail.com>
2019-08-20 09:34:36 -0400

    Fix bugs (time bug and several minor bugs)

--- ai/index.js
+++ ai/index.js
@@ -2,7 +2,7 @@
  * Trains the AI to predict when a bus will arrive at a bus stop.
  *
  * @author Michael O'Donnell
- * @version 0.3.0
+ * @version 0.4.0
  */
 
 "use strict";
@@ -35,7 +35,7 @@ import { exec } from "shelljs";
 async function getAndSaveFile(name) {
   const file = createWriteStream(`./${name}.json`);
   const request = get(
-    `https://bloombus.bloomu.edu/api/download/${name}/geojson`,
+    `https://bloombus.mads.bloomu.edu/api/download/${name}/geojson`,
     response => {
       if (response.statusCode !== 200)
         console.log(`Response status for ${name} was ${response.statusCode}`);
@@ -65,6 +65,7 @@ async function prepareData(inputs, expectedOutputs) {
   console.log("Retrieving data...");
   const garbage = await getData(
     "loops",
+    "loop-stops",
     "historical-logs",
     "stops",
     "intersections"
@@ -74,31 +75,32 @@ async function prepareData(inputs, expectedOutputs) {
   const data = {
     intersections: JSON.parse(readFileSync("./intersections.json", "utf8")),
     loops: JSON.parse(readFileSync("./loops.json", "utf8")),
+    loopStops: JSON.parse(readFileSync("./loop-stops.json", "utf8")),
     historicalLogs: JSON.parse(readFileSync("./historical-logs.json", "utf8")),
     stops: JSON.parse(readFileSync("./stops.json", "utf8"))
   };
 
   // sort data by bus loop
   let sortedHistoricalLogs = {
-    campusLoop: [],
-    downtownLoop: [],
-    latenightLoop: [],
-    walmartLoop: []
+    campus: [],
+    downtown: [],
+    latenight: [],
+    walmart: []
   };
 
   for (log in data.historicalLogs) {
     switch (log.loopKey) {
       case "campus":
-        sortedHistoricalLogs.campusLoop.push(log);
+        sortedHistoricalLogs.campus.push(log);
         break;
       case "downtown":
-        sortedHistoricalLogs.downtownLoop.push(log);
+        sortedHistoricalLogs.downtown.push(log);
         break;
       case "latenight":
-        sortedHistoricalLogs.latenightLoop.push(log);
+        sortedHistoricalLogs.latenight.push(log);
         break;
       case "walmart":
-        sortedHistoricalLogs.walmartLoop.push(log);
+        sortedHistoricalLogs.walmart.push(log);
         break;
       default:
         console.error(`Unsorted loop name present in database: ${log.loopKey}`);
@@ -109,112 +111,171 @@ async function prepareData(inputs, expectedOutputs) {
   // gather data
   function getPoints(member) {
     let pointArr = [];
-    sortedHistoricalLogs[member]["histPoints"].forEach(data => {
-      pointArr.push(_point([data.coordinates[0], data.coordinates[1]]));
-    });
+    for (log of sortedHistoricalLogs[member])
+      for (histPoint of log.histpoints)
+        pointArr.push(
+          _point([histPoint.coordinates[0], histPoint.coordinates[1]])
+        );
     return pointArr;
   }
   function getSpeeds(member) {
     let speedArr = [];
-    sortedHistoricalLogs[member]["histPoints"].forEach(data => {
-      speedArr.push(data.speed);
-    });
+    for (log of sortedHistoricalLogs[member])
+      for (histPoint of log.histpoints) speedArr.push(histPoint.speed);
     return speedArr;
   }
-  function getTimes(member) {
-    let timeArr = [];
-    sortedHistoricalLogs[member]["histPoints"].forEach(data => {
-      timeArr.push(data.timestamp);
-    });
-    return timeArr;
+  function getCollectedTimes(member) {
+    let collectedTimeArr = [];
+    for (log of sortedHistoricalLogs[member])
+      for (histPoint of log.histPoints)
+        collectedTimeArr.push({
+          time: histPoint.timestamp,
+          nextStop: log.nextStop
+        });
+    return collectedTimeArr;
   }
+  function getArrivalTimes(member) {
+    let arrivalTimeArr = [];
+    for (log of sortedHistoricalLogs[member])
+      arrivalTimeArr.push({ arrivalTime: log.arrivalTime, stop: log.nextStop });
+    return arrivalTimeArr;
+  }
+  let loopNames = {
+    campus: "campus",
+    downtown: "downtown",
+    latenight: "latenight",
+    walmart: "walmart"
+  };
   let trainingData = {
     campusLoop: {
+      name: loopNames.campus,
       loop: lineString(data.loops.features[0].geometry.coordinates),
-      points: getPoints("campus"),
-      speeds: getSpeeds("campus"),
-      times: getTimes("campus")
+      points: getPoints(loopNames.campus),
+      speeds: getSpeeds(loopNames.campus),
+      collectedTimes: getCollectedTimes(loopNames.campus),
+      arrivalTimes: getArrivalTimes(loopNames.campus)
     },
     downtownLoop: {
+      name: loopNames.downtown,
       loop: lineString(data.loops.features[2].geometry.coordinates),
-      points: getPoints("downtown"),
-      speeds: getSpeeds("downtown"),
-      times: getTimes("downtown")
+      points: getPoints(loopNames.downtown),
+      speeds: getSpeeds(loopNames.downtown),
+      collectedTimes: getCollectedTimes(loopNames.downtown),
+      arrivalTimes: getArrivalTimes(loopNames.downtown)
     },
     latenightLoop: {
+      name: loopNames.latenight,
       loop: lineString(data.loops.features[l].geometry.coordinates),
-      points: getPoints("latenight"),
-      speeds: getSpeeds("latenight"),
-      times: getTimes("latenight")
+      points: getPoints(loopNames.latenight),
+      speeds: getSpeeds(loopNames.latenight),
+      collectedTimes: getCollectedTimes(loopNames.latenight),
+      arrivalTimes: getArrivalTimes(loopNames.latenight)
     },
     walmartLoop: {
+      name: loopNames.walmart,
       loop: lineString(data.loops.features[w].geometry.coordinates),
-      points: getPoints("walmart"),
-      speeds: getSpeeds("walmart"),
-      times: getTimes("walmart")
+      points: getPoints(loopNames.walmart),
+      speeds: getSpeeds(loopNames.walmart),
+      collectedTimes: getCollectedTimes(loopNames.walmart),
+      arrivalTimes: getArrivalTimes(loopNames.walmart)
     }
   };
+
+  // assemble logs into runs and separate data in loop (do not ask how long it'll be until the bus comes back to stop X and then the next stop)
+  function sortRuns(loopData, stops) {
+    // sort arrivals
+    loopData.sortedByStop = {};
+    for (stop of stops) loopData.sortedByStop[stop] = [];
+    // sort arrivals by stop
+    for (arrival of loopData.arrivalTimes)
+      for (stop of stops)
+        if (arrival.stop === stop) loopData.sortedByStop[stop].push(arrival);
+
+    // sort arrivals by time
+    for (stop of stops)
+      loopData.sortedByStop[stop].sort((a, b) => a.arrivalTime - b.arrivalTime);
+
+    // sort points between arrivals
+    loopData.dataSortedByTime = [];
+    for (stop of stops) {
+      let arr = [];
+      loopData.sortedByStop[stop].forEach((arrivalTime, index) => {
+        loopData.collectedTimes.forEach((time, idx) => {
+          if (
+            index === 0 &&
+            time < arrivalTime &&
+            new Date(time).getDate() === new Date(arrivalTime).getDate()
+          )
+            arr.push({
+              currentPoint: loopData.points[idx],
+              destination: data.stops[stop].geometry.coordinates,
+              speed: loopData.speeds[idx],
+              currentTime: time,
+              arrivalTime: arrivalTime
+            });
+          else if (
+            // this is an else-if to prevent index-out-of-bounds (index of -1)
+            time > loopData.sortedByStop[stop][index - 1] &&
+            time < arrivalTime &&
+            new Date(loopData.sortedByStop[stop][index - 1]).getDate() ===
+              new Date(arrivalTime).getDate()
+          )
+            arr.push({
+              currentPoint: loopData.points[idx],
+              destination: data.stops[stop].geometry.coordinates,
+              speed: loopData.speeds[idx],
+              currentTime: time,
+              arrivalTime: arrivalTime
+            });
+        });
+      });
+      loopData.dataSortedByTime.push(arr);
+    }
+  }
+
+  for (loopData in trainingData)
+    sortRuns(loopData, data.loopStops[loopData.name]);
 
   // get distances between points
   console.log("Analysing data...");
   const unitOfMeasurement = "kilometers";
 
-  for (loop in trainingData) {
-    // snap points to the loop
-    for (point of loop.points) {
-      point = nearestPointOnLine(loop.loop, point, {
+  for (loop of trainingData) {
+    for (entry of loop.dataSortedByTime) {
+      entry.currentPoint = nearestPointOnLine(loop.loop, entry.currentPoint, {
         units: unitOfMeasurement
       });
-    }
-    loop.points.forEach((point, pointIndex) => {
-      point.properties.distances = [];
-      loop.points.forEach((pt, ptIndex) => {
-        if (point !== pt && loop.times[pointIndex] < loop.times[ptIndex]) {
-          const slice = lineSlice(point, pt, loop.loop);
-          let num_of_intersections = 0;
-          for (intersection of data.intersections) {
-            if (
-              booleanPointOnLine(
-                _point(intersection.geometry.coordinates),
-                slice
-              )
-            ) {
-              num_of_intersections++;
-            }
-          }
-          let numOfStops = 0;
-          for (stop in data.stops) {
-            if (booleanPointOnLine(_point(stop.geometry.coordinates), slice)) {
-              numOfStops++;
-            }
-          }
-          num_of_stops--; // booleanPointOnLine will include the target bus stop (remove this line if we want that included)
-          point.properties.distances.push({
-            distance: length(slice, { units: unitOfMeasurement }),
-            numOfIntersections: num_of_intersections,
-            numOfBusStops: numOfStops,
-            pointIndex: ptIndex
-          });
+      entry.destination = nearestPointOnLine(
+        loop.loop,
+        _point(entry.destination),
+        {
+          units: unitOfMeasurement
         }
-      });
-    });
-  }
+      );
+      let slice = lineSlice(entry.currentPoint, entry.destination, loop.loop);
 
-  // prepare data for training
-  console.log("Preparing data...");
-  for (loop in trainingData) {
-    loop.points.forEach((point, pointIndex) => {
-      point.properties.distances.forEach(dist => {
-        inputs.push([
-          dist.distance,
-          dist.numOfIntersections,
-          dist.numOfBusStops,
-          loop.speeds[pointIndex],
-          loop.times[pointIndex]
-        ]);
-        expectedOutputs.push([loop.times[dist.pointIndex]]);
-      });
-    });
+      let numOfBusStops = 0;
+      for (stop of data.stops)
+        if (booleanPointOnLine(_point(stop.geometry.coordinates), slice))
+          numOfBusStops++;
+      numOfBusStops--; // booleanPointOnLine will include the target bus stop (remove this line if we want that included)
+
+      let numOfIntersections = 0;
+      for (intersection of data.intersections)
+        if (
+          booleanPointOnLine(_point(intersection.geometry.coordinates), slice)
+        )
+          numOfIntersections++;
+
+      inputs.push([
+        length(slice, { units: unitsOfMeasurement }), // distance
+        numOfBusStops,
+        numOfIntersections,
+        speed
+      ]);
+
+      expectedOutputs.push([(entry.arrivalTime - entry.currentTime) / 1000]); // predict number of seconds instead of number of milliseconds
+    }
   }
 }
 
@@ -300,7 +361,7 @@ function uploadToServer() {
   console.log("Uploading...");
   const file = createReadStream("model.tar.gz");
   const options = {
-    hostname: "bloombus.bloomu.edu",
+    hostname: "bloombus.mads.bloomu.edu",
     port: 80,
     path: "/ai",
     method: "POST",
